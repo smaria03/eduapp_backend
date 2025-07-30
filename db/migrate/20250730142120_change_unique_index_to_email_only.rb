5a6cf9e4053bfd44510508a86326b7339020f7be
Maria Silaghi <smaria.oana@yahoo.com>
2025-07-30 18:01:00 +0300

    Fix migration: make index removal reversible

--- db/migrate/20250730142120_change_unique_index_to_email_only.rb
+++ db/migrate/20250730142120_change_unique_index_to_email_only.rb
@@ -1,6 +1,6 @@
 class ChangeUniqueIndexToEmailOnly < ActiveRecord::Migration[6.1]
   def change
-    remove_index :users, name: 'index_users_on_email_and_role'
+    remove_index :users, column: [:email, :role]
     add_index :users, :email, unique: true
   end
 end
