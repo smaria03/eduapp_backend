d48ff1ee43d1d0803a24b8d75a1e17f71776e30b
Maria Silaghi <smaria.oana@yahoo.com>
2025-08-27 17:27:00 +0300

    Allow students to view homeworks for their class

--- app/controllers/api/homeworks_controller.rb
+++ app/controllers/api/homeworks_controller.rb
@@ -1,14 +1,27 @@
 module Api
   class HomeworksController < ApplicationController
-    before_action :authorize_teacher!
+    before_action :authorize_teacher!, except: [:index]
+    before_action :authorize_user!, only: [:index]
 
     def index
-      homeworks = Homework
-                  .joins(:assignment)
-                  .where(school_class_subjects: { teacher_id: current_user.id })
+      if current_user.teacher?
+        homeworks = Homework
+                      .joins(:assignment)
+                      .where(school_class_subjects: { teacher_id: current_user.id })
 
-      if params[:assignment_id].present?
-        homeworks = homeworks.where(assignment_id: params[:assignment_id])
+        homeworks = homeworks.where(assignment_id: params[:assignment_id]) if params[:assignment_id].present?
+
+      elsif current_user.student?
+        homeworks = Homework
+                      .joins(:assignment)
+                      .where(school_class_subjects: { school_class_id: current_user.school_class_id })
+
+        if params[:subject_id].present?
+          homeworks = homeworks.where(school_class_subjects: { subject_id: params[:subject_id] })
+        end
+
+      else
+        return render json: { error: 'Unauthorized' }, status: :unauthorized
       end
 
       results = homeworks.map do |homework|
@@ -66,6 +79,12 @@ module Api
       return if current_user&.role == 'teacher'
 
       render json: { error: 'Unauthorized: Teachers only' }, status: :unauthorized
+    end
+
+    def authorize_user!
+      unless current_user&.teacher? || current_user&.student?
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
     end
   end
 end
