2fea3bee56cf98c77af865b50809640cb0fbf7c2
Maria Silaghi <smaria.oana@yahoo.com>
2025-09-03 11:44:18 +0300

    Fix rubocop

--- db/migrate/20250902102456_add_archived_to_school_classes.rb
+++ db/migrate/20250902102456_add_archived_to_school_classes.rb
@@ -1,5 +1,7 @@
 class AddArchivedToSchoolClasses < ActiveRecord::Migration[6.1]
   def change
+    # rubocop:disable Rails/ThreeStateBooleanColumn
     add_column :school_classes, :archived, :boolean
+    # rubocop:enable Rails/ThreeStateBooleanColumn
   end
 end
--- db/migrate/20250902102502_add_graduated_to_users.rb
+++ db/migrate/20250902102502_add_graduated_to_users.rb
@@ -1,5 +1,7 @@
 class AddGraduatedToUsers < ActiveRecord::Migration[6.1]
   def change
+    # rubocop:disable Rails/ThreeStateBooleanColumn
     add_column :users, :graduated, :boolean
+    # rubocop:enable Rails/ThreeStateBooleanColumn
   end
 end
